aa1b2c353afd4c4ea90e6c3bb236b958746d5a17
Rexsect <46561873+Rexsect@users.noreply.github.com>
2019-02-12 11:02:20 +0100

    opp5

--- main_boring.go
+++ main_boring.go
@@ -9,7 +9,7 @@ import (
 // Kode fra Rob Pike https://talks.golang.org/2012/concurrency.slide#20
 func main() {
 	c := make(chan string)
-	go boring.Boring01("boring!")
+	go boring.Boring10("boring!", c)
 	for i := 0;; i++ {
 		fmt.Printf("You say: %q\n", <-c) 
 	}
