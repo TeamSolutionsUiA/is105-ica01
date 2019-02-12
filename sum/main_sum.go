a7193adb5e4d83812d22c6075b18b2f3179f45b5
Rexsect <46561873+Rexsect@users.noreply.github.com>
2019-02-12 12:39:46 +0100

    valg

--- sum/main_sum.go
+++ sum/main_sum.go
@@ -1,21 +1,23 @@
-package sum
+package main
 
-func SumInt8(a, b int8) int8 {
-	return a + b
+import (
+"fmt"	
+"strconv"
+"os";
+"./sum";	
+)
+
+
+func main() {
+v1 :=os.Args;
+v2, err :=strconv.ParseInt(v1[1],10,64);
+v3, err :=strconv.ParseInt(v1[2],10,64);
+
+
+tot :=sum.SumInt64(v2, v3)
+
+fmt.Println(tot);
+fmt.Println(err);
+
 }
 
-func SumFloat64(a, b float64) float64 {
-	return a + b
-}
-
-func SumInt32(a, b int32) int32 {
-	return a + b
-}
-
-func SumInt64(a, b int64) int64 {
-	return a + b
-}
-
-func SumUint32(a, b uint32) uint32 {
-	return a + b
-}
