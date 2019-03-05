22876e09be3e1105437807af29d35bc2f6a83d9b
Are <arefjerm@gmail.com>
2019-03-05 12:40:13 +0100

    3 feilhåndtering

--- sum/main_sum.go
+++ sum/main_sum.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-"fmt"	
-"strconv"
-"os";
-"github.com/TeamSolutionsUiA/ica01/is105-ica02/sum/sum";	
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/TeamSolutionsUiA/is105-ica02/sum/sum"
 )
 
-
 func main() {
-v1 :=os.Args;
-v2, err :=strconv.ParseInt(v1[1],10,64);
-v3, err :=strconv.ParseInt(v1[2],10,64);
+	v1 := os.Args
+	v2, err := strconv.ParseInt(v1[1], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	v3, err := strconv.ParseInt(v1[2], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	tot := sum.SumInt64(v2, v3)
 
-tot :=sum.SumInt64(v2, v3)
-if err==nil {
-fmt.Println(tot);
+	fmt.Println(tot)
 
-}else {
-fmt.Println(err);	
 }
-
-
-
-}
-
