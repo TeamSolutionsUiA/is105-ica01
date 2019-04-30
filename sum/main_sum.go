b75b844db9854d8d754a1899591aba33314d510c
Are <arefjerm@gmail.com>
2019-04-30 12:25:54 +0200

    oppgave 3 v1.5

--- sum/main_sum.go
+++ sum/main_sum.go
@@ -11,17 +11,22 @@ import (
 
 func main() {
 	v1 := os.Args
-	v2, err := strconv.ParseInt(v1[1], 10, 64)
+	i1, err := strconv.ParseInt(v1[1], 10, 64)
+	isFloat := false
+	if err != nil {
+		//printe ut om out of range
+		// parse string og isFloat = true om syntax feil
+		log.Fatal(err)
+	}
+	i2, err := strconv.ParseInt(v1[2], 10, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
-	v3, err := strconv.ParseInt(v1[2], 10, 64)
-	if err != nil {
-		log.Fatal(err)
+	if isFloat{
+		tot := sum.SumFloat64(f1, f2)
+		fmt.Println(tot)
+	} else{
+		tot := sum.SumInt64(i1, i2)
+		fmt.Println(tot)
 	}
-
-	tot := sum.SumInt64(v2, v3)
-
-	fmt.Println(tot)
-
 }
